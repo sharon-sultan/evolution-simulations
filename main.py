77125ca6081206eec2c8def12b54d977600fe93a
EfiHerbst31 <79085273+EfiHerbst31@users.noreply.github.com>
2023-11-08 09:52:35 +0000

    Changed file name

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="main",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to Evolution simulator! 👋")
 
     st.sidebar.success("Select a demo above.")
 
